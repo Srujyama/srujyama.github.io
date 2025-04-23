c6274df8ad80d72ea72a22fe95a100d93642c754
Srujan Yamali <136523182+Srujyama@users.noreply.github.com>
2025-04-23 02:02:57 -0400

    Add files via upload

---
+++ src/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from './Header'
+import './Resume.css' // Dark mode styles
+import {motion} from "framer-motion"; // For this section-specific styling
+
+export default function About() {
+    return (
+        <div className="resume-container">
+            <Header/>
+
+
+            <section className="neural-section">
+                <motion.h1
+                    className="resume-title"
+                    initial={{y: -20, opacity: 0}}
+                    animate={{y: 0, opacity: 1}}
+                    transition={{duration: 0.6, delay: 0.2}}
+                >
+                    <h1 className="neural-title text-fade-in">About Me</h1>
+                </motion.h1>
+
+            </section>
+        </div>
+    )
+}
+
+

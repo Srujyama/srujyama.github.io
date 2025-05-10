16c02c606759e6cfe8d07864041cfae1fb6f997c
Srujyama <srujanyamali@berkeley.edu>
2025-05-10 13:34:01 -0400

    changed Root

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: '/', // this is correct for a user site like yourusername.github.io
+    base: "/",
     plugins: [react()]
 })
